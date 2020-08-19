3a9b7999571a51b207ad860dc902d4267820f9d2
Fer3on <68365999+Fer3onUC@users.noreply.github.com>
2020-08-19 09:58:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,355 @@
+<head>
+    <!-- META -->
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="theme-color" content="#141619">
+
+    <title>Medusa Conquer - Best Private Server</title>
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" href="img/Untitled-1.png">
+
+    <!-- CORE CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/ecommerce.css" rel="stylesheet" />
+    <link href="assets/css/theme.min.css" rel="stylesheet">
+    <link href="assets/css/custom.css" rel="stylesheet">
+    <link href="assets/css/helpers.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700" rel='stylesheet' type='text/css'>
+
+    <!-- PLUGINS -->
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="assets/animate/animate.min.css" rel="stylesheet">
+    <link href="assets/animate/animate.delay.css" rel="stylesheet">
+    <link href="assets/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="assets/ekko-lightbox/ekko-lightbox.min.css" rel="stylesheet">
+
+    <!-- Add jQuery library -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-latest.min.js"></script>
+
+    <!-- Add fancyBox -->
+    <link rel="stylesheet" href="assets/fancybox/source/jquery.fancyboxca96.css?v=2.1.7" type="text/css" media="screen" />
+    <script type="text/javascript" src="assets/fancybox/source/jquery.fancybox.packca96.js?v=2.1.7"></script>
+
+    <!-- Optionally add helpers - button, thumbnail and/or media -->
+    <link rel="stylesheet" href="assets/fancybox/source/helpers/jquery.fancybox-buttons3447.css?v=1.0.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-buttons3447.js?v=1.0.5"></script>
+    <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-mediac924.js?v=1.0.6"></script>
+
+    <link rel="stylesheet" href="assets/fancybox/source/helpers/jquery.fancybox-thumbsf2ad.css?v=1.0.7" type="text/css" media="screen" />
+    <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-thumbsf2ad.js?v=1.0.7"></script>
+
+
+</head>
+
+<body class="fixed-header">
+    <header>
+        <div class="container">
+			<span class="bar hide"></span>
+			<a class="logo"><img src="assets/images/logo.png" class="logoSized" alt=""></a>
+			<nav>
+				<div class="nav-control">
+					<ul>
+						<li><a href="MainPage">Home</a></li>
+						<li><a href="CreateAccount">Register</a></li>
+						<li><a href="Download">Download</a></li>
+						<li><a href="Download">Donate</a></li>
+						<li><a href="Download">Top Rankings</a></li>
+						
+						
+						<li class="dropdown mega-dropdown">
+							<a>Guide</a>
+							<ul class="dropdown-menu mega-dropdown-menu category">
+								<li class="col-md-3">
+									<a href="AboutDonations">
+										<img src="assets/images/product/1.jpg" alt="">
+										<div class="caption">
+											<span class="label label-primary">Donation</span>
+											<h3>How to donate and claim.</h3>
+											<p>Everything related to Donations.</p>
+										</div>
+									</a>
+								</li>
+								<li class="col-md-3">
+									<a href="WareHousePassword">
+										<img src="assets/images/product/1.jpg" alt="">
+										<div class="caption">
+											<span class="label label-danger">SECURITY</span>
+											<h3>How to create/use warehouse password.</h3>
+											<p>Everything related to warehouse password.</p>
+										</div>
+									</a>
+								</li>
+								<li class="col-md-3">
+									<a href="WareHousePassword">
+										<img src="assets/images/product/1.jpg" alt="">
+										<div class="caption">
+											<span class="label label-danger">SECURITY</span>
+											<h3>How to secure your Account.</h3>
+											<p>Everything related to your accounts.</p>
+										</div>
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div class="nav-right">
+				<div class="nav-profile dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="assets/images/avatar2.jpg" alt=""> <span>Guest</span></a>
+					<ul class="dropdown-menu">
+						<li><a href="Login"><i class="fa fa-user"></i> Login</a></li>
+						<!--
+						<li><a href="#"><i class="fa fa-heart"></i> Likes <span class="label label-info">32</span></a></li>
+						<li><a href="#"><i class="fa fa-gamepad"></i> Games</a></li>
+						<li><a href="#"><i class="fa fa-gear"></i> Settings</a></li>
+						<li class="divider"></li>
+						<li><a href="login.html"><i class="fa fa-power-off"></i> Sign Out</a></li>
+						-->
+					</ul>
+				</div>
+				<!--
+				<div class="nav-dropdown dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> <span class="label label-danger">3</span></a>
+					<ul class="dropdown-menu">
+						<li class="dropdown-header"><i class="fa fa-bell"></i> You have 5 new games</li>
+						<li><a href="#">Alien Isolation</a></li>
+						<li><a href="#">Witcher 3 <span class="label label-success">XBOX</span></a></li>
+						<li><a href="#">Last of Us</a></li>
+						<li><a href="#">Uncharted 4 <span class="label label-primary">PS4</span></a></li>
+						<li><a href="#">GTA 5 <span class="label label-warning">PC</span></a></li>
+						<li class="dropdown-footer"><a href="#">View all games</a></li>
+					</ul>
+				</div>
+				-->
+			</div>
+		</div>
+
+    </header>
+      <section class="hero height-600 hero-game" style="background-image: url(assets/images/header.jpg);"></section>
+    
+    
+    
+        <div class="tab-select text-center">
+            <div class="container">
+                <ul class="nav nav-tabs">
+                    <li class=""><a><font color='green' class='link online'>ONLINE</font></a></li>
+                    <li><a>ACCOUNTS: <font color='#0de81ead'>1,621</font></a></li>
+                    <li><a>CHARACTERS: <font color='#0de81ead'>1,621</font></a></li>
+                    <li><a>ONLINE PLAYERS: <b><font color='#7fad11'>1,621</font></b></a></li>
+                    <li><a>MAX ONLINE SINCE LAST RESTART: <font color='#b75353'>1,621</font></a></li>
+
+                </ul>
+            </div>
+        </div>
+    
+    <!-- end .site-header -->
+
+    
+
+	<div class="site-content" role="main">
+		<section class="bg-grey-50 border-bottom-1 border-grey-200 relative">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="title">
+							<h4>WELCOME TO CONQUER LEGACY!</h4>
+							<br />
+							<h3 style="font-size:17px;">
+								<p>
+									For years i have searched for a classic Conquer Online server that had all the useful features of the modern game, no matter how hard I tried i could not find one.
+									I wanted to bring back the memories without the clunky feel of 5095 clients, so i decided to make my own, and it has been a huge success so far!
+									You will not find a better more complete classic server with mind blowing performance and ping. We are the best and we are here to stay!
+								</p>
+							</h3>
+						</div>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+						<div class="card card-hover">
+							<div class="card-img">
+								<img src="assets/images/product/1.jpg" alt="">
+								<div class="category"><span class="label label-danger">EVENT</span></div>
+								<div class="meta"><span class="label label-warning">WEEKLY</span></div>
+							</div>
+							<div class="caption">
+								<h3 class="card-title"><a>Super Guild War</a></h3>
+								<ul><li>Sunday 15:00 to 19:00</li></ul>
+								<ul><li>Guild war halos, 50,000 CPs and LordToken.</li></ul>
+							</div>
+						</div>
+					</div>
+							
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+						<div class="card card-hover">
+							<div class="card-img">
+								<img src="assets/images/product/1.jpg" alt="">
+								<div class="category"><span class="label label-danger">EVENT</span></div>
+								<div class="meta"><span class="label label-warning">Daily</span></div>
+							</div>
+							<div class="caption">
+								<h3 class="card-title"><a>Class PK War</a></h3>
+								<ul><li>Every day 15:00 expect Sunday.</li></ul>
+								<ul><li>Top class halos and 1,000 CPs.</li></ul>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+						<div class="card card-hover">
+							<div class="card-img">
+								<img src="assets/images/product/1.jpg" alt="">
+								<div class="category"><span class="label label-danger">EVENT</span></div>
+								<div class="meta"><span class="label label-warning">HOURLY</span></div>
+							</div>
+							<div class="caption">
+								<h3 class="card-title"><a>Last Man Standing</a></h3>
+								<ul><li>Every hour xx:20</li></ul>
+								<ul><li>1,000 CPs for the winner.</li></ul>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+						<div class="card card-hover">
+							<div class="card-img">
+								<img src="assets/images/product/4.jpg" alt="">
+								<div class="category"><span class="label label-danger">EVENT</span></div>
+								<div class="meta"><span class="label label-warning">HOURLY</span></div>
+							</div>
+							<div class="caption">
+								<h3 class="card-title"><a>Kill The Captian</a></h3>
+								<ul><li>Every hour xx:20</li></ul>
+								<ul><li>2,000 CPs for the winner.</li></ul>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+						<div class="card card-hover">
+							<div class="card-img">
+								<img src="assets/images/product/1.jpg" alt="">
+								<div class="category"><span class="label label-danger">EVENT</span></div>
+								<div class="meta"><span class="label label-warning">HOURLY</span></div>
+							</div>
+							<div class="caption">
+								<h3 class="card-title"><a>Freeze War (Team/Skill bassed)</a></h3>
+								<ul><li>Every hour xx:20</li></ul>
+								<ul><li>1,000 CPs for all winners.</li></ul>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+						<div class="card card-hover">
+							<div class="card-img">
+								<img src="assets/images/product/1.jpg" alt="">
+								<div class="category"><span class="label label-danger">EVENT</span></div>
+								<div class="meta"><span class="label label-warning">HOURLY</span></div>
+							</div>
+							<div class="caption">
+								<h3 class="card-title"><a>Team Death Match (Team/Skill bassed)</a></h3>
+								<ul><li>Every hour xx:20</li></ul>
+								<ul><li>1,000 CPs for all winners.</li></ul>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="row margin-">
+					<div class="col-md-12">
+						<table style="font-size:14px;" class="table">
+							<thead>
+								<tr><td><strong>Category:</strong> 2.0 Conquer Online private server.</td></tr>
+								<tr><td><strong>Classes:</strong>  Archer - Warrior - Trojan - Taoist.</td></tr>
+								<tr><td><strong>Stats:</strong> Max level 137 - Max Reborn 2 - Max Compose +12.</td></tr>
+								<tr><td><strong>Rates:</strong> 2x Experience 2x Weapon Prof - 2x Item Drops - 2x Mining + Compose - DB 4K Kills - Meteor 1K Kills.</td></tr>
+								<tr><td><strong>Host:</strong> Quad hosted USA Dedicated servers, you will not find a better performing server than ours.</td></tr>
+								<tr><td><strong>Type Of Server:</strong> Classic, we have taken the best features from the modern game and added them to classic.</td></tr>
+								<tr><td><strong>Most Exciting Features:</strong> Full arena qualifier - Bosses - Events - Reincarnation - Pure Skills & Much More!</td></tr>
+								<tr><td><strong>Events:</strong> Super Guild War - Elite PK - Team PK - Skill Team PK - Class PK - Pole Domination - Last Man Standing & More.</td></tr>
+								<tr><td><strong>Launch Date:</strong> May 2020.</td></tr>
+							</thead>
+						</table>
+					</div>
+				</div>
+				
+			</div>
+		</section>
+	</div>
+
+	<!-- end .site-content -->
+
+    <!-- footer -->
+    <footer>
+        <div class="footer-bottom">
+            <div class="container">
+				<div class="col-md-6">
+					<strong style="font-size:13px;">Copyright © 2020 <a class="text-primary">ConquerLegacy</a> All Rights Reserved.</strong>
+				</div>
+				<div class="col-md-6">
+					<strong style="font-size:13px;">Built by <a href="https://www.facebook.com/PreMadee" target="_blank" class="text-success">PreMade</a>.</strong>
+				</div>
+                
+			</div>
+        </div>
+    </footer>
+    <a href="https://discord.com/invite/conquerlegacy"><img src="assets/images/discord.gif" style="bottom: 0px; right: 10px; position: fixed; padding: 5px;border-radius: 4px 1px 0; width: 87px;cursor:pointer;"></a>	<!-- /.footer -->
+    <!-- Javascript -->
+    <script src="assets/js/jquery-1.11.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/ekko-lightbox/ekko-lightbox.min.js"></script>
+    <script src="assets/js/core.min.js"></script>
+    <script src="assets/owl-carousel/owl.carousel.min.js"></script>
+    <script>
+        (function ($) {
+            "use strict";
+            var owl = $(".owl-carousel");
+
+            owl.owlCarousel({
+                autoPlay: 3000,
+                items: 1, //4 items above 1000px browser width
+                itemsDesktop: [1000, 3], //3 items between 1000px and 0
+                itemsTablet: [600, 1], //1 items between 600 and 0
+                itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+            });
+
+            $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
+
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 350) {
+                    $('body').addClass('fixed-tab');
+                } else {
+                    $('body').removeClass('fixed-tab');
+                }
+            });
+        })(jQuery);
+    </script>
+    <script>
+
+        var myVar = setInterval(myTimer, 1000);
+        function myTimer() {
+            var d = new Date(), displayDate;
+            if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+                displayDate = d.toLocaleTimeString('pt-BR');
+            } else {
+                displayDate = d.toLocaleTimeString('pt-BR', {timeZone: 'New York City' });
+            }
+            
+            document.getElementById("demo").innerHTML = displayDate;
+        }
+    </script>
+
+</body>
+</html>
